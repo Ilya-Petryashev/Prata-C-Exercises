5658d861f132d6d3e6320e45f774a43d57f407ea
Ilya-Petryashev <petryashev.is@gmail.com>
2022-06-20 03:18:09 +0300

    ch4 ready
    Добавлен готовый проект с заданиями из 4-й главы

--- 4th chapter/Chapter 4/Chapter 4.cpp
+++ 4th chapter/Chapter 4/Chapter 4.cpp
@@ -2,6 +2,7 @@
 #include <stdio.h>
 #include <stdint.h>
 #include <string.h>
+#include <float.h>
 
 void doTask1(void);
 void doTask2(void);
@@ -54,41 +55,97 @@ int main()
 
 void doTask1(void)
 {
-	float pisya = 1.234;
-	printf("%0.2f\n", pisya);
+	char name[15], surname[15];
+	printf("Enter your name and surname: ");
+	scanf("%s %s", name, surname);
+
+	printf("You are %s %s\n", surname, name);
 }
 
 void doTask2(void)
 {
-	printf("Incorrect input, shutting down\n");
+	char name[15];
+	printf("Enter your name: ");
+	scanf("%s", name);
+
+	printf("\"%s\"\n", name);
+	printf("\"%20s\"\n", name);
+	printf("\"%-20s\"\n", name);
+	printf("%*s\n", strlen(name)+3, name);
 }
 
 void doTask3(void)
 {
-	printf("Incorrect input, shutting down\n");
+	float value;
+	printf("Enter float value: ");
+	scanf("%f", &value);
+
+	printf("Input is %.1f or %.1e\n", value, value);
+	printf("Input is %+.3f or %+.3e\n", value, value);
 }
 
 void doTask4(void)
 {
-	printf("Incorrect input, shutting down\n");
+	char name[15];
+	float heightInCM;
+	printf("Enter your name and height in centimeters: ");
+	scanf("%s %f", name, &heightInCM);
+
+	printf("%s, your height is %.2f meters\n", name, heightInCM/100.0);
 }
 
 void doTask5(void)
 {
-	printf("Incorrect input, shutting down\n");
+	float downloadSpeed, fileSize, downloadTime;
+	printf("Enter download speed (in megabits per sec) and file size (in megabytes): ");
+	scanf("%f %f", &downloadSpeed, &fileSize);
+
+	downloadTime = fileSize * 8.0 / downloadSpeed;
+	printf("Download of a %.2f MB file at a speed of %.2f Mbps will take %.2f sec(-s)\n",
+		fileSize, downloadSpeed, downloadTime);
 }
 
 void doTask6(void)
 {
-	printf("Incorrect input, shutting down\n");
+	char name[15], surname[15];
+	int8_t nameLength, surnameLength;
+	printf("Enter your name and surname: ");
+	scanf("%s %s", name, surname);
+
+	nameLength = strlen(name);
+	surnameLength = strlen(surname);
+	printf("%s %s\n", name, surname);
+	printf("%*d %*d\n", nameLength, nameLength, surnameLength, surnameLength);
+	printf("%s %s\n", name, surname);
+	printf("%-*d %-*d\n", nameLength, nameLength, surnameLength, surnameLength);
 }
 
 void doTask7(void)
 {
-	printf("Incorrect input, shutting down\n");
+	float floatValue;
+	double doubleValue;
+	floatValue = 1.0 / 3.0;
+	doubleValue = 1.0 / 3.0;
+	
+	printf("float: %.4f, double: %.4f\n", floatValue, doubleValue);
+	printf("float: %.12f, double: %.12f\n", floatValue, doubleValue);
+	printf("float: %.16f, double: %.16f\n", floatValue, doubleValue);
+	printf("Max precision for float: %d, max precision for double: %d\n", FLT_DIG, DBL_DIG);
 }
 
 void doTask8(void)
 {
-	printf("Incorrect input, shutting down\n");
+	float numMiles, numGallons, numLiters, numKilometers, measureUSA, measureEU;
+	const float gallonsToLiters = 3.785, milesToKilometers = 1.609;
+	printf("Enter the amount of miles traveled and the amount of gallons of gas spent: ");
+	scanf("%f %f", &numMiles, &numGallons);
+
+	numLiters = gallonsToLiters * numGallons;
+	numKilometers = milesToKilometers * numMiles;
+	printf("You traveled %.1f kilometers and spent %.1f liters of gas\n", numKilometers, numLiters);
+
+	measureUSA = numMiles / numGallons;
+	measureEU = numLiters / (100 * numKilometers);
+	printf("In American system: %.1f miles per gallon\n", measureUSA);
+	printf("In European system: %.1f liters per 100 km\n", measureEU);
 }
